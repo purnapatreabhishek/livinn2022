64e8457a37911e86b129634565ca8d84ef9f5e8b
Abhishek Purnapatre <purnapatreabhishek1@gmail.com>
2022-05-30 15:48:05 +0530

    updated files

--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app');
-const connectMongo = require('./utils/connect-mongo');
-
-connectMongo();
-
-app.listen(process.env.PORT || 5000, () => {
-  console.log('Server Started');
-});
